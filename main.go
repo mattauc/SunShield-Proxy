e965fc546fa68896ce513959a3617b9c11d8569d
Matthew <63832577+mattauc@users.noreply.github.com>
2024-06-29 14:24:10 +1000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+
+    "github.com/gin-gonic/gin"
+)
+
+func Weather(c *gin.Context) {
+
+	lat := c.Query("lat")
+    lon := c.Query("lon")
+	exclude := c.Query("exclude")
+	units := c.Query("units")
+    
+    fmt.Println("Latitude:", lat)
+    fmt.Println("Longitude:", lon)
+	fmt.Println("Exclude:", exclude)
+	fmt.Println("Units:", units)
+
+	uri := "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + exclude + "&units=" + units +"&appid=" + os.Getenv("OPEN_WEATHER_TOKEN")
+	//Get Request to URI
+    response, error := http.Get(uri)
+    if error != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": error.Error()})
+    }
+    defer response.Body.Close()
+
+
+    body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
+        return
+	}
+
+    c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+    c.String(http.StatusOK, string(body))
+}
+
+func main() {
+    r := gin.Default()
+    r.POST("/api/weather", Weather)
+    r.Run(":8000")
+}
